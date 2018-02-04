c6b92dbcc8acfabc7ba99c311335b7387c5a4ba1
jdeluyck <sub+github@kcore.org>
2018-02-04 17:24:31 +0100

    Update to use usb/tty instead of wireless

--- watermeter/watermeter.ino
+++ watermeter/watermeter.ino
@@ -1,12 +1,3 @@
-// Arduino code for water meter readout and sending over wireless link using nRF24L01 module
-// J.Heyman
-// nRF24L01 code adapted from: http://blog.riyas.org/2014/08/raspberry-pi-as-nrf24l01-base-station-internet-connected-wireless.html
-
-#include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
-#include <RF24_config.h>
-
 int value;
 int previousvalue;
 
@@ -20,42 +11,23 @@ unsigned long nbTurns=0;
 #define HIGH_THRESHOLD 60
 #define LOW_THRESHOLD  30
 
-// MOSI is on pin 11
-// MISO is on pin 12
-// SCK is on pin 13
-// CE is on pin 9, CSN is on pin 10
-RF24 radio(9,10);
-
-const uint64_t pipe = { 0xF0F0F0F0D2LL };
 char message[30];
 
 void setup() {
-
-  //nRF24 configurations
-  radio.begin();
-  radio.setChannel(0x4c);
-  radio.setAutoAck(1);
-  radio.setRetries(15,15);
-  radio.setDataRate(RF24_250KBPS);
-  radio.setPayloadSize(32);
-  radio.openWritingPipe(pipe);
-  radio.startListening();
+  Serial.begin(115200);
 }
- 
-void sendMessage() {
-    // send data over wireless link
-    radio.stopListening();
-    radio.write(&message,strlen(message));
-    radio.startListening(); 
-} 
- 
+
+void sendMessage(char message[30]) {
+  Serial.println(message);
+}
+
 void loop(){
   value = analogRead(A0)/4;
   nbRead++;
   
   // For debug / calibration
   //sprintf(message, "val:%d", value);
-  //sendMessage();
+  //sendMessage(message);
   
   if ((currentstate == SILVER) && (value > HIGH_THRESHOLD) && (previousvalue > HIGH_THRESHOLD)) {
     currentstate = RED;
@@ -65,10 +37,10 @@ void loop(){
     currentstate = SILVER;
     nbTurns++;
     sprintf(message, "water:top:%d", nbTurns);
-    sendMessage();  
+    sendMessage(message);  
   } else if ((nbRead % 50) == 0) {
     sprintf(message, "water:alive");
-    sendMessage(); 
+    sendMessage(message); 
   }
   
   // Loop at 10 Hz
